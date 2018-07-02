2ad50cae8fd62ce5d736abc430e916e5aab4114f
pankaja92 <pankajanc@gmail.com>
2018-07-02 23:10:47 +0530

    Authorization done

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    googleClientId :  process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    mongoUser : process.env.MONGO_USER,
+    mongoPass : process.env.MONGO_PASS,
+    cookieKey : process.env.COOKIE_KEY
+}
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const authRoutes = require('./routes/authRoutes');
+require('./services/passport');
+require('./models/user');
+require('dotenv').config()
+const keys = require('./config/keys');
 
+mongoose.connect(`mongodb://${keys.mongoUser}:${keys.mongoPass}@ds139219.mlab.com:39219/emailey-dev`)
+
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send({hi :"there"});
-})
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 app.listen(port, ()=> console.log(`App started on port ${port}`));
+
